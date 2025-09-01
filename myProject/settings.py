b89b2ab17bd6b05918bb5b8171dc98d0e24d4772
TheAyushThakur <ayushthakur6005@gmail.com>
2025-09-01 13:49:24 +0530

    Installed and initialised allauth for login/logout/signup into this project

--- myProject/settings.py
+++ myProject/settings.py
@@ -38,7 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     'feed',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = 'myProject.urls'
@@ -124,3 +129,22 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SITE_ID= 1
+LOGIN_URL='/login/'
+LOGIN_REDIRECT_URL='/'
+ACCOUNT_AUTHENTICATION_MEHTOD='email'
+ACCOUNT_CONFIRM_EMAIL_ON_GET=True
+ACCOUNT_EMAIL_VERIFICATION='optional'
+ACCOUNT_SIGNUP_FIELDS = ['email*', 'username*', 'password1*', 'password2*']
+ACCOUNT_LOGIN_ON_EMAIL_CONFIRMATION=True
+ACCOUNT_LOGIN_ON_GET=True
+ACCOUNT_LOGIN_ON_PASSWORD_RESET= True
+ACCOUNT_LOGOUT_REDIRECT= '/'
+ACCOUNT_PRESERVE_USERNAME_CASING= False
+ACCOUNT_SESSION_REMEMBER=True
+ACCOUNT_USERNAME_MIN_LENGTH= 3
+AUTHENTICATION_BACKENDS=(
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend'
+)
