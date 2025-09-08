b8675fcd2c945533e77f35de19bc8cc93fbde359
TheAyushThakur <ayushthakur6005@gmail.com>
2025-09-08 12:16:11 +0530

    Following and Unfollowing Logic

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -62,5 +62,25 @@ $(document).on("click", ".js-toggle-modal", function(e) {
 })
 .on("click", ".js-follow", function(e){
     e.preventDefault();
-    console.log("Clicked!")
+    action= $(this).attr("data-action")
+    $.ajax({
+        type: 'POST',
+        url: $(this).data("url"),
+        data: {
+            action: action,
+            username: $(this).data("username"),
+        },
+        success: (data) => {
+            $(".js-follow-text").text(data.wording)
+            if (action == "follow"){
+                $(this).attr("data-action", "unfollow")
+            }
+            else{
+                $(this).attr("data-action", "follow")
+            }
+        },
+        error: (error) => {
+            console.warn(error)
+        }
+    });
 })
